dec4cbb05f1990cb22e412e4111e2ace76dc3bf8
zaka634 <zaka634@gmail.com>
2017-09-07 22:30:00 -0400

    Create init.el

---
+++ init.el
@@ -0,0 +1,37 @@
+(require 'package)
+
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(linum-relative-current-symbol "")
+ '(package-selected-packages (quote (real-auto-save evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+ 
+(require 'evil)
+(evil-mode t)
+
+
+(global-linum-mode t)
+(linum-relative-global-mode t)
+(require 'linum-relative)
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
+
