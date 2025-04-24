0d0c84ccf3c4d8b91a40bad597562fb9709f298d
zaka634 <zaka634@gmail.com>
2025-04-24 18:29:48 -0400

    Create remap.lua

---
+++ nvim/lua/zaka634/remap.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = ","
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
+vim.keymap.set("n", "<leader>v", "v$h", { noremap = true })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
