f7c37e36d0ef95caffb7cf4f259af33c218fd68b
zaka634 <zaka634@gmail.com>
2025-04-24 18:30:09 -0400

    Create packer.lua

---
+++ nvim/lua/zaka634/packer.lua
@@ -0,0 +1,29 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {
+      'nvim-telescope/telescope.nvim',
+      requires = { { 'nvim-lua/plenary.nvim' } },
+  }
+
+  use {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      run = 'make',
+      config = function()
+          require('telescope').load_extension('fzf')
+      end
+  }
+
+  use {
+      'numToStr/Comment.nvim',
+      config = function()
+          require('Comment').setup()
+      end
+  }
+end)
+
