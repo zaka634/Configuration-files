3e53941ddd79d705a4e24f2a68ad48f9f1d790f9
zaka634 <zaka634@gmail.com>
2025-04-24 22:41:39 -0400

    Create easymotion.lua

---
+++ nvim/after/plugin/easymotion.lua
@@ -0,0 +1,5 @@
+vim.api.nvim_set_keymap('n', '<leader>f', '<Plug>(easymotion-bd-f)', {})
+vim.api.nvim_set_keymap('x', '<leader>f', '<Plug>(easymotion-bd-f)', {})
+
+vim.api.nvim_set_keymap('n', '<leader>w', '<Plug>(easymotion-bd-w)', {})
+vim.api.nvim_set_keymap('x', '<leader>w', '<Plug>(easymotion-bd-w)', {})
