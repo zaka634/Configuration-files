c92a8698e549a024970ba440da19bac9d2e2040b
zaka634 <zaka634@gmail.com>
2025-04-24 22:41:59 -0400

    Update packer.lua

--- nvim/lua/zaka634/packer.lua
+++ nvim/lua/zaka634/packer.lua
@@ -4,26 +4,29 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use {
-      'nvim-telescope/telescope.nvim',
-      requires = { { 'nvim-lua/plenary.nvim' } },
-  }
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { { 'nvim-lua/plenary.nvim' } },
+    }
 
-  use {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      run = 'make',
-      config = function()
-          require('telescope').load_extension('fzf')
-      end
-  }
+    use {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        run = 'make',
+        config = function()
+            require('telescope').load_extension('fzf')
+        end
+    }
 
-  use {
-      'numToStr/Comment.nvim',
-      config = function()
-          require('Comment').setup()
-      end
-  }
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+
+    use 'easymotion/vim-easymotion'
+
 end)
 
