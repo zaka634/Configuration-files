01149efde30b2febd2f42a97104150917de7046c
zaka634 <zaka634@gmail.com>
2025-04-24 22:42:24 -0400

    Update telescope.lua

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,10 +4,3 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope: Find 
 vim.keymap.set('n', '<leader>fg', builtin.live_grep,  { desc = 'Telescope: Live Grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers,    { desc = 'Telescope: Buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags,  { desc = 'Telescope: Help Tags' })
-
-require('telescope').setup{
-  defaults = {
-    sorting_strategy = "ascending",
-    file_ignore_patterns = { "node_modules", ".git/" },
-  },
-}
