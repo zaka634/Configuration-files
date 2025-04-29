f58c9de0b078fdf7163d285cc0d218d58d12b497
zaka634 <zaka634@gmail.com>
2025-04-28 22:24:19 -0400

    Update telescope.lua

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,19 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope: Find 
 vim.keymap.set('n', '<leader>fg', builtin.live_grep,  { desc = 'Telescope: Live Grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers,    { desc = 'Telescope: Buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags,  { desc = 'Telescope: Help Tags' })
+
+vim.keymap.set('n', '<leader>fb', function()
+  require('telescope').extensions.file_browser.file_browser({
+    hidden = true,           -- show hidden files
+    respect_gitignore = false -- show everything even if .gitignore hides it
+  })
+end, { desc = 'File Browser (with hidden files)' })
+
+vim.keymap.set('n', '<leader>fe', function()
+  require('telescope').extensions.file_browser.file_browser({
+    path = "%:p:h", -- current buffer's directory
+    hidden = true,
+    respect_gitignore = false,
+    select_buffer = true
+  })
+end, { desc = 'File Browser at current file' })
